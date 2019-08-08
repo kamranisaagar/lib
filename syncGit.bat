bbbe20b381edc53adf98285e06b9596fa316704c
kamranisaagar <kamranisaagar@gmail.com>
2019-08-08 23:49:21 +1000

    Update syncGit.bat

--- syncGit.bat
+++ syncGit.bat
@@ -1,4 +1,4 @@
-start /min runas /savecred /user:administrator mpulseStop.bat
+start /wait runas /savecred /user:administrator mpulseStop.bat
 timeout /t 5 /nobreak
 
 echo "Syncing with Git"
@@ -6,10 +6,12 @@ git reset --hard origin/master
 git clean -f
 git pull
 
-start /min runas /savecred /user:administrator mpulseStart.bat
+start /wait runas /savecred /user:administrator mpulseStart.bat
 
 @ECHO OFF
 set CLASSPATH=.
 set CLASSPATH=%CLASSPATH;
 cd C:\merchantpulse\lib
 start /B javaw -jar -Dconfig.home="c:/merchantpulse" MpulseUI-1.0-SNAPSHOT.jar
+
+exit
