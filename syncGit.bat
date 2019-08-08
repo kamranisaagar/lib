f003e94fecaade5f350927114eaeec1766b9a836
kamranisaagar <kamranisaagar@gmail.com>
2019-08-08 23:44:03 +1000

    Update syncGit.bat

--- syncGit.bat
+++ syncGit.bat
@@ -1,8 +1,9 @@
-net stop MpulseJob
+start /min runas /savecred /user:administrator mpulseStop.bat
+timeout /t 5 /nobreak
 
 echo "Syncing with Git"
 git reset --hard origin/master
 git clean -f
 git pull
 
-net start MpulseJob
+start /min runas /savecred /user:administrator mpulseStart.bat
