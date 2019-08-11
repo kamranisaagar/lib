ddc0aba0985b91a79549a38ebcbdc0babb84429f
Saagar <kamranisaagar@gmail.com>
2019-08-11 16:09:24 +1000

    new changes

---
+++ gitSync.bat
@@ -0,0 +1,8 @@
+net stop MpulseJob
+
+echo "Syncing with Git"
+git reset --hard origin/master
+git clean -f
+git pull
+
+net start MpulseJob
--- runUI.bat
+++ runUI.bat
@@ -1,13 +1,3 @@
-start /min runas /savecred /user:administrator mpulseStop.bat
-timeout /t 5 /nobreak
-
-echo "Syncing with Git"
-git reset --hard origin/master
-git clean -f
-git pull
-
-start /min runas /savecred /user:administrator mpulseStart.bat
-
 @ECHO OFF
 set CLASSPATH=.
 set CLASSPATH=%CLASSPATH;
